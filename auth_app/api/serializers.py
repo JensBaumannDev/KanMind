316d73e030ff30d22877a40fee08f8402320c82b
JensBaumannDev <baumannjens@outlook.de>
2026-08-05 18:13:14 +0200

    feature/login
    -add login endpoint with token authentication

--- auth_app/api/serializers.py
+++ auth_app/api/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 from rest_framework import serializers
 
 
@@ -37,3 +38,22 @@ class RegistrationSerializer(serializers.ModelSerializer):
             first_name=first_name,
             last_name=last_name,
         )
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True, style={"input_type": "password"})
+
+    def validate(self, data):
+        email = data.get("email")
+        password = data.get("password")
+
+        user = authenticate(request=self.context.get("request"), username=email, password=password)
+
+        if not user:
+            raise serializers.ValidationError("Invalid email or password.")
+        if not user.is_active:
+            raise serializers.ValidationError("The user is currently deactivated.")
+
+        data["user"] = user
+        return data
--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from auth_app.api.views import RegistrationView
+from auth_app.api.views import RegistrationView, LoginView
 
 urlpatterns = [
-    path('registration/', RegistrationView.as_view())
+    path('registration/', RegistrationView.as_view()),
+    path('login/', LoginView.as_view())
 ]
