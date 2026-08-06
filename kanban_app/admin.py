325b907e44658760f6e9c09dd82047ad3b741b32
JensBaumannDev <baumannjens@outlook.de>
2026-08-06 19:59:52 +0200

    feature/kanban-admin
    - register kanban_app models in Django admin

--- kanban_app/admin.py
+++ kanban_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Board, Task, Comment
 
 # Register your models here.
+
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
