b3fd2eada35fc59c7b3e91fb0ac2717196028590
JensBaumannDev <baumannjens@outlook.de>
2026-08-10 14:04:55 +0200

    feature/board-permissions
    - add board access
    permissions and owner field

--- kanban_app/api/views.py
+++ kanban_app/api/views.py
@@ -1,16 +1,25 @@
 from rest_framework import viewsets
 from kanban_app.models import Board
-from .serializers import BoardSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from django.contrib.auth.models import User
 from rest_framework.response import Response
+from django.db.models import Q
+from .serializers import BoardSerializer
+from .permissions import IsBoardMember
 
 
 class BoardViewSet(viewsets.ModelViewSet):
-    queryset = Board.objects.all()
     serializer_class = BoardSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsBoardMember]
+
+    def get_queryset(self):
+        return Board.objects.filter(
+            Q(members__id=self.request.user.id) | Q(owner__id=self.request.user.id)
+        )
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class EmailCheckView(APIView):
