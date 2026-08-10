bdf0684e29e85759ad465d7664952632d32333ce
JensBaumannDev <baumannjens@outlook.de>
2026-08-10 17:59:43 +0200

    feature/task-model-prep
    - add task creator field and explicit related_names

--- kanban_app/models.py
+++ kanban_app/models.py
@@ -7,7 +7,9 @@ from django.contrib.auth.models import User
 class Board(models.Model):
     title = models.CharField(max_length=250)
     members = models.ManyToManyField(User)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owned_boards")
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="owned_boards"
+    )
 
     def __str__(self):
         return self.title
@@ -31,11 +33,14 @@ class Task(models.Model):
         ("high", "High"),
     ]
 
-    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name="tasks")
     title = models.CharField(max_length=250)
     description = models.CharField(max_length=250)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES)
     priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES)
+    created_by = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, related_name="created_tasks"
+    )
     assignee = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -61,8 +66,10 @@ class Task(models.Model):
 
 
 class Comment(models.Model):
-    task = models.ForeignKey(Task, on_delete=models.CASCADE)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="authored_comments"
+    )
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
     content = models.CharField(max_length=300)
 
